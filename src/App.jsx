2ae31d614d57cea748da4cadc72c06989a01c62b
matisagardia <matisagardia7@gmail.com>
2023-02-09 19:51:45 -0300

    redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router/AppRouter'
+import { store } from './store/store.js'
 
 export const App = () => {
 
   return (
-    <BrowserRouter>
-      <AppRouter/>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <AppRouter/>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
