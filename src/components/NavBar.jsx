b711d64d63a12e1256233aba9cfce991150b220c
matisagardia <matisagardia7@gmail.com>
2023-02-09 19:36:25 -0300

    routes finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/formaAppLogo.png'
-import { CreateQuestion } from './CreateQuestion'
-import { QuestionList } from './QuestionList'
 
 export const NavBar = () => {
 
+  const navigate = useNavigate();
+
+  const handleClickForm = () => {
+      navigate("/form");
+  }
+
+  const handleClickQuestions = () => {
+    navigate("/");
+}
 
   return (
     <>
 
         <div className="buttonsContainer">
             <img src={logo} alt="logo" className="logo" />
-            <button className='questionsList'>Listado de preguntas</button>
-            <button className='createQuestion'>Crear pregunta</button>
+            <button className='questionsList' onClick={handleClickQuestions}>Listado de preguntas</button>
+            <button className='createQuestion' onClick={handleClickForm}>Crear pregunta</button>
         </div>
 
     </>
